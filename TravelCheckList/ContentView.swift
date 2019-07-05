30648a0fe7f83554afbbfc3614e5146428c280b5
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-05 15:21:56 +0300

    Initial Commit

---
+++ TravelCheckList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TravelCheckList
+//
+//  Created by Atil Samancioglu on 5.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
